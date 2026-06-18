7611836dd005c48f298c32d56f3dd989f402f503
djrxbqm99 <djrxbqm99@gmail.com>
2026-06-18 12:40:42 +0900

    sping 실습

---
+++ ch04/src/main/java/org/example/ch04/dao/User4DAO.java
@@ -0,0 +1,27 @@
+package org.example.ch04.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.ch04.dto.User4DTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// @Mapper는 MyBatis API 호출 컴포넌트 선언
+@Mapper
+@Repository
+public interface User4DAO {
+
+    // 기본 CRUD 메서드
+    public void insert(User4DTO dto);
+    public User4DTO select(String userid);
+    public List<User4DTO> selectAll();
+    public void update(User4DTO dto);
+    public void delete(String userid);
+
+
+}
+
+
+
+
---
+++ ch04/src/main/java/org/example/ch04/service/User2Service.java
@@ -0,0 +1,47 @@
+package org.example.ch04.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ch04.dao.User2DAO;
+import org.example.ch04.dto.User2DTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class User2Service {
+
+    // 주입 대상 속성에 final 선언해서 RequiredArgsConstructor으로 객체 주입
+    private final User2DAO dao;
+
+
+
+
+    // DAO 호출 메서드
+    public void register(User2DTO dto){
+        dao.insert(dto);
+
+    }
+
+
+    public User2DTO getById(String userid){
+        return dao.select(userid);
+    }
+
+
+    public List<User2DTO> getAll(){
+        return dao.selectAll();
+    }
+
+
+    public void modify(User2DTO dto){
+        dao.update(dto);
+    }
+
+
+    public void remove(String userid){
+        dao.delete(userid);
+    }
+
+
+}
